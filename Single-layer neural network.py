aa0b0e12b3dbad30ee21831fc72e1fd6db2154af
N0ktis <2010zalex@gmail.com>
2021-03-24 03:33:49 +0300

    Rework SLNN

--- Single-layer neural network.py
+++ Single-layer neural network.py
@@ -1,163 +1,103 @@
 from math import exp
 import numpy as np
+from numba import jit
 
 
 class Perceptron:
     Et = 0.3
 
-    def __init__(self, N: int, activation_func, d_activation_func):
+    def __init__(self, N: int, d_activation_func):
         self.N = N
-        self.activation_func = activation_func
+        # self.activation_func = activation_func
         self.d_activation_func = d_activation_func
-        self.W = np.array([0 for i in range(np.sqrt(N) + 1)])
+        self.W = np.array([0 for i in range(N + 1)])
+        self.X = self.__get_X(N)
+
+  #  @jit
+    def learn(self, func):
+        E = -1
+        epoch = 0
+        while E != 0:
+            curr_F = self.__get_curr_F(self.N, self.W)
+            print(curr_F)
+            E = self.__get_E(curr_F, func)
+            print(E, epoch)
+
+            for i in range(2 ** self.N):
+                net = self.__get_net(self.W, self.X[i])
+                delta = int(func[i]) - int(curr_F[i])
+                self.W[0] = self.__new_w(self.W[0], delta, True, net)
+                for j in range(1, self.N + 1):
+                    self.W[j] = self.__new_w(self.W[j], delta, self.X[i][j - 1], net)
+            epoch += 1
+            print(epoch, '%', self.W)
+
+   # @jit
+    def __new_w(self, w, delta: int, x: bool, net: int):
+        #rint(w + self.Et * delta * x * self.d_activation_func(net), 'h')
+        return w + self.Et * delta * x * self.d_activation_func(net)
 
     @staticmethod
-    def get_net(w, l):
-        # print(w,'@',l)
-        net = w[0]
-        for i in range(1, len(w)):
-            # net += w[i] * (1 if X[l][i] is True else 0)
-            net += w[i] * X[l][i - 1]
+  #  @jit
+    def __get_X(N: int):
+        X = np.empty((2 ** N, N), bool)
+        X.fill(False)
+        X[0] = np.array([False, False, False, False])
+        for i in range(1, 2 ** N):
+            pos = N - 1
+            buf_i = i
+            while buf_i != 0:
+                if (buf_i % 2) == 1:
+                    X[i][pos] = True
+                else:
+                    X[i][pos] = False
+                pos -= 1
+                buf_i = buf_i // 2
+        return X
+
+    @staticmethod
+ #   @jit
+    def __get_net(W, X) -> int:
+        net = W[0]
+        for i in range(1, len(W)):
+            net += W[i] * X[i - 1]
+            print(W[i],X[i-1])
+        print(net,'b')
         return net
 
     @staticmethod
-    def get_E(S, F):
+#    @jit
+    def __get_E(S, F) -> int:
         E = 0
         for i in range(len(S)):
             E += (0 if S[i] == F[i] else 1)
         return E
 
-    @staticmethod
-    def get_new_S(W):
-        new_F = []
-        for l in range(len(X)):
-            new_F.append(True if get_net(W, l) >= 0 else False)
-        return new_F
-
-    def learn(self, func):
-        E = 13
-        k = 0
-        while E != 0:
-            S = get_new_S(W)
-            print(S)
-            E = get_E(S, F)
-            print(E, k)
-
-            for i in range(len(S)):
-                net = get_net(W, i)
-                delta = F[i] - S[i]
-                W[0] = W[0] + new_w(W[0], delta, True) * 1
-                for j in range(1, len(W)):
-                    W[j] = W[j] + new_w(W[j], delta, X[i][j - 1]) * 1
-            k += 1
-            print(k, '%', W)
+ #   @jit
+    def __get_curr_F(self, N: int, W):
+        curr_F = np.empty(2 ** (len(W) - 1), bool)
+        for i in range(2 ** N):
+            curr_F[i] = (True if self.__get_net(W, self.X[i]) >= 0 else False)
+        return curr_F
 
 
-def linear_activation(W, l):
-    return True if get_net(W, l) >= 0 else False
+def linear_activation(net):
+    return True if net >= 0 else False
 
 
-def sigma_activation(W, l):
-    return 1 / (1 + exp(-get_net(W, l)))
+def d_linear_activation(net: int) -> int:
+    return 1
 
 
-Et = 0.3
-X = [[False, False, False, False],
-     [False, False, False, True],
-     [False, False, True, False],
-     [False, False, True, True],
-     [False, True, False, False],
-     [False, True, False, True],
-     [False, True, True, False],
-     [False, True, True, True],
-     [True, False, False, False],
-     [True, False, False, True],
-     [True, False, True, False],
-     [True, False, True, True],
-     [True, True, False, False],
-     [True, True, False, True],
-     [True, True, True, False],
-     [True, True, True, True]]
-
-"""
-def new_w(w, d, x):
-    return w + Et * d * x
-"""
+def sigma_activation(net: int):
+    return 1 / (1 + exp(net))
 
 
-def new_w(w, d, x):
-    return Et * d * x
+def d_sigma_activation(net: int) -> float:
+    return (exp(net)) / (1 + exp(net)) ** 2
 
 
-def get_net(w, l):
-    # print(w,'@',l)
-    net = w[0]
-    for i in range(1, len(w)):
-        # net += w[i] * (1 if X[l][i] is True else 0)
-        net += w[i] * X[l][i - 1]
-    return net
-
-
-def get_E(S, F):
-    E = 0
-    for i in range(len(S)):
-        E += (0 if S[i] == F[i] else 1)
-    return E
-
-
-def get_new_S(W):
-    new_F = []
-    for l in range(len(X)):
-        new_F.append(True if get_net(W, l) >= 0 else False)
-    return new_F
-
-
-def linear_activation(W, l):
-    return True if get_net(W, l) >= 0 else False
-
-
-def sigma_activation(W, l):
-    return 1 / (1 + exp(-get_net(W, l)))
-
-
-W = [0, 0, 0, 0, 0]
-S = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
 F = [False, False, False, True, False, False, True, True, False, False, True, True, False, False, True, True]
 # F = [False, False, False, True, False, False, False, True, False, False, False, True, False, False, False, False]
-
-E = 13
-k = 0
-while E != 0:
-    S = get_new_S(W)
-    print(S)
-    E = get_E(S, F)
-    print(E, k)
-
-    for i in range(len(S)):
-        net = get_net(W, i)
-        delta = F[i] - S[i]
-        W[0] = W[0] + new_w(W[0], delta, True) * 1
-        for j in range(1, len(W)):
-            W[j] = W[j] + new_w(W[j], delta, X[i][j - 1]) * 1
-    k += 1
-
-print(k, '%', W)
-W = [0, 0, 0, 0, 0]
-S = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-E = 13
-k = 0
-while E != 0:
-    S = get_new_S(W)
-    print(S)
-    E = get_E(S, F)
-    print(E, k)
-
-    for i in range(len(S)):
-        net = get_net(W, i)
-        delta = F[i] - S[i]
-        W[0] = W[0] + new_w(W[0], delta, True) * sigma_activation(W, i) * (1 - sigma_activation(W, i))
-        for j in range(1, len(W)):
-            W[j] = W[j] + new_w(W[j], delta, X[i][j - 1]) * sigma_activation(W, i) * (1 - sigma_activation(W, i))
-    k += 1
-
-print(k, W)
+A = Perceptron(4, d_linear_activation)
+A.learn(np.array(F))
--- test.py
+++ test.py
@@ -1,9 +1,48 @@
 from math import exp
+import numpy as np
+import itertools
 
-a=True-False
-b=True
-print(b*0.5)
+print(list(itertools.combinations_with_replacement([True, False], 4)))
+print(list(itertools.combinations_with_replacement([False, True], 4)))
+a = True - False
+b = True
+print(b * 0.5)
 print(a)
 
 print(exp(2))
 print([0 for i in range(3)])
+
+
+def binary(a):
+    s = ''
+    while a != 0:
+        s += str (a % 2)
+        a = a // 2
+    print(s)
+
+binary(1)
+binary(0)
+binary(2)
+binary(7)
+
+
+def get_X(N: int):
+    X = np.empty((2 ** N, N), bool)
+    for i in range(2 ** N):
+        pos = N - 1
+        ii=i
+        while ii != 0:
+            print(i)
+            if (ii % 2) == 1:
+                X[i][pos] = True
+            else:
+                X[i][pos] = False
+            pos -= 1
+            ii = ii // 2
+    return X
+
+print(get_X(4))
+
+
+
+print(False-True)
