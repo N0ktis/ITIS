5ef616459d92e00ee85e5c134aa3dd1140a4ea5a
N0ktis <2010zalex@gmail.com>
2021-04-01 16:38:06 +0300

    Update labs

--- Single-layer neural network.py
+++ Single-layer neural network.py
@@ -1,49 +1,95 @@
+from itertools import combinations
 from math import exp
+
 import numpy as np
-from numba import jit
+import plotly.graph_objs as go
 
 
 class Perceptron:
     Et = 0.3
 
-    def __init__(self, N: int, d_activation_func):
+    def __init__(self, N: int, d_activation_func, bool_func):
         self.N = N
-        # self.activation_func = activation_func
         self.d_activation_func = d_activation_func
-        self.W = np.array([0 for i in range(N + 1)])
-        self.X = self.__get_X(N)
+        self.bool_func = bool_func
+        self.W = np.array([0. for i in range(N + 1)])
+        self.X_set = self.get_X_set(N)
+        # self.func = None
 
-  #  @jit
-    def learn(self, func):
+    # @jit
+    def learn(self, func, X_set, max_epoch=np.inf):
+        # self.func = func
+        E_stat = []
         E = -1
         epoch = 0
-        while E != 0:
-            curr_F = self.__get_curr_F(self.N, self.W)
-            print(curr_F)
+        while E != 0 or epoch == max_epoch:
+            curr_F = self.__get_curr_F(X_set, self.W)
+            #print(curr_F)
             E = self.__get_E(curr_F, func)
-            print(E, epoch)
+            E_stat.append(E)
+            #print(E, epoch)
 
-            for i in range(2 ** self.N):
-                net = self.__get_net(self.W, self.X[i])
+            for i in range(len(X_set)):
+                net = self.__get_net(self.W, X_set[i])
                 delta = int(func[i]) - int(curr_F[i])
-                self.W[0] = self.__new_w(self.W[0], delta, True, net)
+                self.W[0] = self.__new_w(self.W[0], delta, True, net, i)
                 for j in range(1, self.N + 1):
-                    self.W[j] = self.__new_w(self.W[j], delta, self.X[i][j - 1], net)
+                    self.W[j] = self.__new_w(self.W[j], delta, X_set[i][j - 1], net, i)
             epoch += 1
-            print(epoch, '%', self.W)
+            #print(epoch, '%', self.W)
+        # self.graph_builder(E_stat)
+        return E, epoch, self.W, E_stat
 
-   # @jit
-    def __new_w(self, w, delta: int, x: bool, net: int):
-        #rint(w + self.Et * delta * x * self.d_activation_func(net), 'h')
+    def get_min_vector(self, func, max_epoch=400):
+        min_len_comb = 2 ** self.N + 1
+        min_comb = None
+        # min_epoch = max_epoch
+        curr_e = -1
+        min_w = None
+        j = 0
+        for i in range(2 ** self.N, 1, -1):
+            for X_comb in combinations(self.get_X_set(self.N), i):
+                j += 1
+                self.W.fill(0.)
+                min_func = np.empty(len(X_comb), bool)
+                min_func.fill(False)
+                for k in range(i):
+                    min_func[k] = self.bool_func(X_comb[k])
+                E, epoch, w, e_stat = self.learn(min_func, X_comb, max_epoch)
+                print(self.W)
+                if self.__get_E(self.__get_curr_F(self.X_set, self.W), func) == 0 and len(X_comb) <= min_len_comb:
+                    min_len_comb = len(X_comb)
+                    print('!')
+                    min_comb = X_comb
+                    curr_e = epoch
+                    min_w = w
+                    min_e = e_stat
+                print(j, E, X_comb)
+        print(curr_e, min_comb, min_w, min_e)
+
+    @staticmethod
+    def graph_builder(E_stat):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter({'x': [i for i in range(len(E_stat))], 'y': E_stat, 'name': 'E'}))
+        fig.update_layout(legend_orientation="h",
+                          legend=dict(x=.5, xanchor="center"),
+                          title='График суммарной ошибки НС по эпохам обучения',
+                          xaxis_title='Эпохи',
+                          yaxis_title='Суммарная ошибка',
+                          margin=dict(l=0, r=0, t=30, b=0))
+        fig.show()
+
+    # @jit
+    def __new_w(self, w, delta: int, x: bool, net: int, i):
         return w + self.Et * delta * x * self.d_activation_func(net)
 
     @staticmethod
-  #  @jit
-    def __get_X(N: int):
+    # @jit
+    def get_X_set(N: int):
         X = np.empty((2 ** N, N), bool)
         X.fill(False)
-        X[0] = np.array([False, False, False, False])
-        for i in range(1, 2 ** N):
+        # X[0] = np.array([False, False, False, False])
+        for i in range(2 ** N):
             pos = N - 1
             buf_i = i
             while buf_i != 0:
@@ -56,33 +102,33 @@ class Perceptron:
         return X
 
     @staticmethod
- #   @jit
+    # @jit
     def __get_net(W, X) -> int:
         net = W[0]
         for i in range(1, len(W)):
             net += W[i] * X[i - 1]
-            print(W[i],X[i-1])
-        print(net,'b')
         return net
 
     @staticmethod
-#    @jit
+    # @jit
     def __get_E(S, F) -> int:
+        # print(S,'2',F)
         E = 0
-        for i in range(len(S)):
+        for i in range(len(F)):
             E += (0 if S[i] == F[i] else 1)
         return E
 
- #   @jit
-    def __get_curr_F(self, N: int, W):
-        curr_F = np.empty(2 ** (len(W) - 1), bool)
-        for i in range(2 ** N):
-            curr_F[i] = (True if self.__get_net(W, self.X[i]) >= 0 else False)
+    # @jit
+    @staticmethod
+    def __get_curr_F(X_set, W):
+        curr_F = np.empty(len(X_set), bool)
+        for i in range(len(X_set)):
+            curr_F[i] = (True if Perceptron.__get_net(W, X_set[i]) >= 0 else False)
         return curr_F
 
 
-def linear_activation(net):
-    return True if net >= 0 else False
+def linear_activation(net) -> int:
+    return 1 if net >= 0 else 0
 
 
 def d_linear_activation(net: int) -> int:
@@ -97,7 +143,16 @@ def d_sigma_activation(net: int) -> float:
     return (exp(net)) / (1 + exp(net)) ** 2
 
 
+def get_bool_func(X_vector):
+    return (X_vector[0] + X_vector[1] + X_vector[3]) * X_vector[2]
+
+
 F = [False, False, False, True, False, False, True, True, False, False, True, True, False, False, True, True]
 # F = [False, False, False, True, False, False, False, True, False, False, False, True, False, False, False, False]
-A = Perceptron(4, d_linear_activation)
-A.learn(np.array(F))
+A = Perceptron(4, d_linear_activation, get_bool_func)
+# A.learn(np.array(F), A.get_X_set(4))
+#A.learn(np.array([False, True, False, True]),[[False, False, True, False], [False, False, True, True], [True, True, False, False],[True, True, True, False]])
+B = Perceptron(4, d_sigma_activation, get_bool_func)
+# B.learn(np.array(F), B.get_X_set(4))
+
+A.get_min_vector(np.array(F))
--- test.py
+++ test.py
@@ -1,24 +1,16 @@
+import itertools
 from math import exp
 import numpy as np
-import itertools
 
-print(list(itertools.combinations_with_replacement([True, False], 4)))
-print(list(itertools.combinations_with_replacement([False, True], 4)))
-a = True - False
-b = True
-print(b * 0.5)
-print(a)
-
-print(exp(2))
-print([0 for i in range(3)])
 
 
 def binary(a):
     s = ''
     while a != 0:
-        s += str (a % 2)
+        s += str(a % 2)
         a = a // 2
-    print(s)
+    #print(s)
+
 
 binary(1)
 binary(0)
@@ -30,7 +22,7 @@ def get_X(N: int):
     X = np.empty((2 ** N, N), bool)
     for i in range(2 ** N):
         pos = N - 1
-        ii=i
+        ii = i
         while ii != 0:
             print(i)
             if (ii % 2) == 1:
@@ -41,8 +33,11 @@ def get_X(N: int):
             ii = ii // 2
     return X
 
+
+def get_bool_func(X_vector):
+    return (X_vector[0] + X_vector[1] + X_vector[3]) * X_vector[2]
+
+
 print(get_X(4))
-
-
-
-print(False-True)
+for i in get_X(4):
+    print(get_bool_func(i))
